23ddab7488d71dbafd74915f590cc9b4beb54ca3
gontijobernardo <gontijo.bernardo@gmail.com>
2016-01-16 10:10:27 -0600

    Replace server with a dumbed down content

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT ||3000;
 
 app.get('/hello', function(req, res){
-    res.send('hello world2');
+    res.send('hello world');
 });
 app.listen(port, ipaddress);
 
