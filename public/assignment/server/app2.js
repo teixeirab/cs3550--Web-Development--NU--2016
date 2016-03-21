2989ed1426eeb12633cc2356a870b3f08c88fede
gontijobernardo <gontijo.bernardo@gmail.com>
2016-03-21 19:43:46 -0400

    Updated Package.json in order for the server in openshift not to crash

---
+++ public/assignment/server/app2.js
@@ -0,0 +1,8 @@
+module.exports = function(app, uuid) {
+    var userModel = require("./models/user.model.server.js")(uuid);
+    var formModel = require("./models/form.model.server.js")(uuid);
+    //a
+    var userService = require("./services/user.service.server.js")(app, formModel, userModel);
+    var formService = require("./services/form.service.server.js")(app, formModel, userModel);
+    var fieldService = require("./services/field.service.server.js")(app, formModel);
+}
