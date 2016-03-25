5b897b89f0009727a81bab14d738b4cefe0cf36d
gontijobernardo <gontijo.bernardo@gmail.com>
2016-03-24 23:58:24 -0400

    Improved the projects home page navigation

--- public/project/prototype/client/view/home/home.controller.js
+++ public/project/prototype/client/view/home/home.controller.js
@@ -4,7 +4,30 @@
         .module("SimulyApp")
         .controller("HomeController", HomeController);
 
-    function HomeController($scope, $location) {
-        $scope.$location = $location;
+    function HomeController(UserService, $location, $rootScope) {
+        var vm = this;
+
+        vm.login = login;
+
+        function init() {
+        }
+        init();
+
+        function login(user) {
+            if(!user) {
+                return;
+            }
+            UserService
+                .login({
+                    username: user.username,
+                    password: user.password
+                })
+                .then(function(response){
+                    if(response.data) {
+                        UserService.setCurrentUser(response.data);
+                        $location.url("/profile");
+                    }
+                });
+        }
     }
 })();
