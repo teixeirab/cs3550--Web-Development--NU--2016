7746b57cc3f8072e93ada67be2419443c786f7d2
gontijobernardo <gontijo.bernardo@gmail.com>
2016-04-07 23:45:22 -0400

    small fix

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ app.use(session({
 }));
 app.use(express.static(__dirname+ '/public'));
 
-//require("./public/assignment/server/app.js")(app, uuid, db, mongoose);
-require("./public/project/implementation/server/app.js")(app, uuid, db, mongoose);
+require("./public/assignment/server/app.js")(app, uuid, db, mongoose);
+//require("./public/project/implementation/server/app.js")(app, uuid, db, mongoose);
 //require("./public/experiments/omdb_mongo_db/server/app.js")(app, db, mongoose);
 //require("./public/experiments/omdb/server/app.js")(app);
 
