84e8698f7d6449951cd2911188f52a8c7852a6fd
gontijobernardo <gontijo.bernardo@gmail.com>
2016-04-12 21:54:17 -0400

    finished hw5

--- server.js
+++ server.js
@@ -43,8 +43,8 @@ app.use(passport.session());
 app.use(express.static(__dirname+ '/public'));
 
 
-//require("./public/assignment/server/app.js")(app, uuid, db, mongoose);
-//require("./public/project/implementation/server/app.js")(app, uuid, db, mongoose);
+require("./public/assignment/server/app.js")(app, uuid, db, mongoose);
+require("./public/project/implementation/server/app.js")(app, uuid, db, mongoose);
 //require("./public/experiments/omdb_mongo_db/server/app.js")(app, db, mongoose);
 //require("./public/experiments/omdb/server/app.js")(app);
 
