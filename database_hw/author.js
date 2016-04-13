46afd6e0a28cfac43fc2bd0f4e7fff3087c32acb
gontijobernardo <gontijo.bernardo@gmail.com>
2016-04-13 12:59:00 -0400

    test

--- database_hw/author.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(mongoose) {
-    var AuthorSchema = mongoose.Schema({
-        firstName: String,
-        lastName: String,
-        date_of_birth: Date
-    }, {collection: 'database.author'});
-    return AuthorSchema;
-};
--- server.js
+++ server.js
@@ -41,7 +41,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(__dirname+ '/public'));
 
-console.log(process.env.OPENSHIFT_NODEJS_PORT)
 
 require("./public/assignment/server/app.js")(app, uuid, db, mongoose);
 require("./public/project/implementation/server/app.js")(app, uuid, db, mongoose);
